58615b6d2fe66f70fed15b1cabea6caf05c4bf82
KJM <tdcian@hexlant.com>
2020-02-19 10:33:40 +0900

    Initial Commit

---
+++ SearchBarDelgate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBarDelgate
+//
+//  Created by hexlant_01 on 2020/02/19.
+//  Copyright © 2020 hexlant_01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
